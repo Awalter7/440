cb91e4c1f5baf5f5505a66952a7f3715e237593d
Awalter7 <89409100+Awalter7@users.noreply.github.com>
2025-10-05 12:01:27 -0400

    w & h on canvas

--- components/three/canvas/Lander.jsx
+++ components/three/canvas/Lander.jsx
@@ -53,7 +53,7 @@ export function LanderScene({
         ...style,
       }}
     >
-      <Canvas shadows camera={{ position, fov }}  style={{backgroundColor: "transparent", height: "100vh", width: "100vw", zIndex: 2}} gl={{ preserveDrawingBuffer: true}} eventSource={document.getElementById('root')} eventPrefix="client">
+      <Canvas shadows camera={{ position, fov }}  style={{backgroundColor: "transparent", height: "100%", width: "100%", zIndex: 2}} gl={{ preserveDrawingBuffer: true}} eventSource={document.getElementById('root')} eventPrefix="client">
           <fog attach="fog" color="black" near={1} far={3.5} />
           <Environment environmentIntensity={.2} files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
           <OrbitalRig >
