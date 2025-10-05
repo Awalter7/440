d711f44b79a52ac51bbbf4c2fbe5ab489da05871
Awalter7 <89409100+Awalter7@users.noreply.github.com>
2025-10-05 06:38:03 -0400

    Build Update

--- components/three/scenes/Lander.jsx
+++ components/three/scenes/Lander.jsx
@@ -1,19 +1,57 @@
-// Custom hook to get window scroll position
-import {Canvas} from '@react-three/fiber'
-import {Environment} from '@react-three/drei'
-import OrbitalRig from '../rigs/OrbitalRig'
-import Backdrop from '../objects/Backdrop'
-import Guitar from '../objects/Guitar'
+import React, { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import OrbitalRig from "../rigs/OrbitalRig";
+import Backdrop from "../objects/Backdrop";
+import Guitar from "../objects/Guitar";
 
-export const LanderScene = ({ position = [0, 0, 2], fov = 25 }) => (
-  <Canvas shadows camera={{ position, fov }}  style={{backgroundColor: "transparent", height: "100vh", width: "100vw", zIndex: 2}} gl={{ preserveDrawingBuffer: true}} eventSource={document.getElementById('root')} eventPrefix="client">
-      <fog attach="fog" color="black" near={1} far={3.5} />
-      <OrbitalRig >
-        <Guitar position={[0, -.30, 0]}/>
-        <Backdrop position={[0, -.295, 0]}/>
-        <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
-      </OrbitalRig>
-  </Canvas>
-)
+/**
+ * Plasmic-compatible 3D scene component.
+ * You can drop this into a Plasmic project and control its container size.
+ */
+export function LanderScene({
+  position = [0, 0, 2],
+  fov = 25,
+  className,
+  style,
+}) {
+  const containerRef = useRef(null);
 
+  return (
+    <div
+      ref={containerRef}
+      className={className}
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        background: "transparent",
+        ...style,
+      }}
+    >
+      <Canvas
+        shadows
+        camera={{ position, fov }}
+        style={{
+          backgroundColor: "transparent",
+          width: "100%",
+          height: "100%",
+        }}
+        gl={{ preserveDrawingBuffer: true }}
+        eventSource={containerRef}
+        eventPrefix="client"
+      >
+        <fog attach="fog" color="black" near={1} far={3.5} />
+        <OrbitalRig>
+          <Guitar position={[0, -0.3, 0]} />
+          <Backdrop position={[0, -0.295, 0]} />
+          <Environment
+            files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr"
+          />
+        </OrbitalRig>
+      </Canvas>
+    </div>
+  );
+}
 
+export default LanderScene;
