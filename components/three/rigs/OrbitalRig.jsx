93b349a1ceeaf578749f563ab40d4fcfb35d9378
Awalter7 <89409100+Awalter7@users.noreply.github.com>
2025-10-05 14:21:20 -0400

    Scene Update

--- components/three/rigs/OrbitalRig.jsx
+++ components/three/rigs/OrbitalRig.jsx
@@ -10,8 +10,8 @@ export default function OrbitalRig({ children }) {
   const snap = useSnapshot(state)
 
   useFrame((state, delta) => {
-    easing.damp3(state.camera.position, [snap.intro ? -state.viewport.width / 15: 0, .35, .45], 0.25, delta)
-    easing.dampE(group.current.rotation, [-state.pointer.x / 20, -state.pointer.y / 20, Math.PI / 4], 0.25, delta)
+    easing.damp3(state.camera.position, [snap.intro ? -state.viewport.width / 10: 0, .35, 1], 0.25, delta)
+    easing.dampE(group.current.rotation, [Math.PI / 20, -state.pointer.y / 20, Math.PI / 4], 0.25, delta)
   })
   return <group ref={group}>{children}</group>
 }
