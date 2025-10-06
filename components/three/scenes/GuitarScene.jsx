1f8517955b299c3034b1f44f4c5e3c3b32d029d9
Awalter7 <89409100+Awalter7@users.noreply.github.com>
2025-10-06 16:24:03 -0400

    Update GuitarScene.jsx

--- components/three/scenes/GuitarScene.jsx
+++ components/three/scenes/GuitarScene.jsx
@@ -13,7 +13,7 @@ export default function GuitarScene({ animatedPosition, animatedRotation }) {
       <MovingSpot depthBuffer={depthBuffer} color="#ffffff" position={[.50, 1.5, .2]} lookAt={[0, .25, 0]} attenuation={.5} anglePower={10} intensity={25} distance={10}/>
       <MovingSpot depthBuffer={depthBuffer} color="#ffffff" position={[0, 1, 1]} lookAt={[0, .25, 0]} attenuation={1.5} angle={.35} anglePower={30} intensity={10} move={false}/>
       <MovingSpot depthBuffer={depthBuffer} color="#ffffff" position={[0, 0, 1]} lookAt={[0, .25, 0]} attenuation={1.5} angle={.35} anglePower={30} intensity={10} move={false}/>
-      <AnimatedGuitar animatedPosition={animatedPosition} animatedRotation={animatedRotation}/>
+      <Guitar animatedPosition={animatedPosition} animatedRotation={animatedRotation}/>
       <Backdrop position={[0, -.004, -.1]} rotation={[Math.PI / 2, 0, 0]}/>
     </>
   );
