a438ddacea2bb32d6b42eecd3f7f7a6410d2d5cc
Awalter7 <89409100+Awalter7@users.noreply.github.com>
2026-01-18 16:16:24 -0500

    Update gravity.jsx

--- components/effects/physics/gravity.jsx
+++ components/effects/physics/gravity.jsx
@@ -23,7 +23,7 @@ export function GravityWrapper({
       grabCursor={grabCursor}
       addTopWall={addTopWall}
       autoStart={autoStart}
-      className={"w-full h-full"}
+      className={className}
       {...restProps}
     >
       {children}
